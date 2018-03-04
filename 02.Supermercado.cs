5d5e1826551ad8a139128d223defad75c65c809d
mario-valladares <37032177+mario-valladares@users.noreply.github.com>
2018-03-03 21:18:56 -0600

    Update 02.Supermercado.cs
    no se si le iras a modificar algo mas

--- 02.Supermercado.cs
+++ 02.Supermercado.cs
@@ -13,14 +13,50 @@ derecho los clientes cuya compra supere los $30.
 
 Notal: No se sabe la cantidad de clientes que llegará al supermercado.  **/
 
-using System;
-
-{
-    class supermercado
-    {
-        static void Main(string[] args)
+uusing System;
+class supermercado {
+    static void Main(string []args) {
+		int sino=0;
+				Console.WriteLine("|=====================================================================|");
+                Console.WriteLine("| Para iniciar digite --> 1, para salir digite --> 0                  |");
+			    Console.WriteLine("|=====================================================================|");
+         sino = Convert.ToInt32(Console.ReadLine());
+		 while (sino > 0) {
+        Console.Write("Introduzca el monto total de su compra: ");
+        double compra = Convert.ToDouble(Console.ReadLine());
+        
+        if (compra > 30){
+           
+                Random rnd = new Random();
+                int tombola = rnd.Next(1,6); /* Genera un numero entre 1 y 5 */
+                switch (tombola) {
+                    case 1:
+                        Console.WriteLine("Felicidades, usted se ha ganado 1 shampoo!!");
+                    break;
+                    case 2:
+                        Console.WriteLine("Felicidades, usted de ha ganado 1 paquete de 3 jabones de baño!!");
+                    break;
+                    case 3:
+                        Console.WriteLine("Felicidades, usted se ha ganado 1 pasta de dientes!!");
+                    break;
+                    case 4:
+                        Console.WriteLine("Felicidades, usted se ha ganado 1 bolsa de detergente!!");
+                    break;
+                    case 5:
+                        Console.WriteLine("Felicidades, usted se ha ganado 1 caja de cereal de chocolate!!");
+                    break;
+                }
+			   
+				
+            }
+        else
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Su compra no califica para la tombola ya que no es superior a $30.");
         }
+		       Console.WriteLine("|=====================================================================|");
+               Console.WriteLine("|Desea ingresar otra compra? digite --> 1, para salir digite --> 0    |");
+			   Console.WriteLine("|=====================================================================|");
+         sino = Convert.ToInt32(Console.ReadLine());
     }
+}
 }
